bbc89b766ee0fafddb0fce003e0bffacd386c9d9
rominadouk <douk.romina@gmail.com>
2023-03-14 14:39:17 -0700

    a commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,43 @@ import PackingList from './PackingList';
 const App = () => {
   const [list, setList] = useState([])
 
+    //use setlist Dat in handle change
+    const [listData, setListData] = useState(
+      {
+        category:[""],
+        top:[""], 
+        bottoms:[""],
+        shoes:[""],
+        headgear: [""],
+        accessories: [""]
+      }
+    );
+  
+    const [lists, setLists] = useState([])
+  
+    //Get packed items database 
+    const allPackingItems = () => {
+      axios.get('http://localhost:3000/location').then(response => {
+        setLists(response.data)
+        console.log(response.data)
+      });
+    };
+  
+  
+    //Delete
+    const deleteItem = (lists) => {
+      axios.delete(`http://localhost:3000/location/${lists._id}`).then(()=> {
+        axios.get('http://localhost:3000/location').then((response) => {
+          setLists(response.data)
+        })
+      })
+    };
+  
+    useEffect(()=> {
+      allPackingItems()
+    },[])
     
+  //Update
 
   return (
     <div>
