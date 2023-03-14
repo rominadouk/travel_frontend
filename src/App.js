ff947368cd62d17f79098b4c4a767fa16ad7e25a
McKee Thatcher <mckeethatcher22@gmail.com>
2023-03-14 13:33:26 -0700

    pimped the hell outa this bish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
+import Weather from './Weather';
 
 const App = () => {
   const [listArray, setListArray] = useState(
@@ -40,46 +41,10 @@ const App = () => {
 
   //Update
 
-
-
-  function getWeatherMessage(city, temperature, humidity, wind, forecast) {
-    if (temperature.current < 50) {
-      return `Looks like it's chilly in ${city} today! Don't forget to bring a jacket.`;
-    } else if (temperature.current < 30) {
-      return `Brrr! It's freezing in ${city}! Bring a heavy coat today.`;
-    } else if (forecast.includes('rain')) {
-      return `Looks like it might rain in ${city} today. Don't forget your umbrella!`;
-    } else if (temperature.current > 80 && humidity > 60) {
-      return `It's hot and humid in ${city} today! Stay hydrated and wear breathable clothing.`;
-    } else if (wind.speed > 15) {
-      return `Looks like it's windy in ${city} today! Hold onto your hats.`;
-    } else if (temperature.current > 90) {
-      return `Wow, it's scorching in ${city} today! Stay indoors or seek shade if possible.`;
-    } else if (temperature.current > 75 && forecast.includes('thunderstorms')) {
-      return `Be careful if you're out and about in ${city} today - there's a chance of thunderstorms.`;
-    } else {
-      return `Looks like a beautiful day in ${city}! Pack light and enjoy the sunshine.`;
-    }
-  }
-
   return (
     <div>
-      {/* <h1>{weatherData.city}, {weatherData.state}</h1> */}
-      {/* <p>Current Temperature: {weatherData.temperature.current}°F</p> */}
-      {/* <p>High: {weatherData.temperature.high}°F</p> */}
-      {/* <p>Low: {weatherData.temperature.low}°F</p> */}
-      {/* <p>Humidity: {weatherData.humidity}%</p> */}
-      {/* <p>Wind Speed: {weatherData.wind.speed} mph {weatherData.wind.direction}</p> */}
       <button onClick={allPackingItems}>Get Packing Items</button>
-      <ul>
-      {listArray.map((list) => {
-        return (
-          <div>
-            <li>{list.category}</li>
-          </div>
-        )
-      })}
-      </ul>
+      <Weather />
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState({
+    city: '',
+    state: '',
+    temperature: {
+      current: 0,
+      high: 0,
+      low: 0
+    },
+    humidity: 0,
+    wind: {
+      speed: 0,
+      direction: 0
+    },
+    forecast: ''
+  });
+
+  const [cityName, setCityName] = useState('');
+  const [cityList, setCityList] = useState([
+    'New York', 
+    'Los Angeles', 
+    'Chicago', 
+    'Houston', 
+    'Phoenix'
+  ]);
+
+  useEffect(() => {
+    if (cityName) {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=6add74d6b76b0b938510da6c2c14f624`)
+        .then(response => {
+          const data = response.data;
+          setWeatherData({
+            city: data.name,
+            state: data.sys.country,
+            temperature: {
+              current: Math.round(data.main.temp),
+              high: Math.round(data.main.temp_max),
+              low: Math.round(data.main.temp_min)
+            },
+            humidity: data.main.humidity,
+            wind: {
+              speed: Math.round(data.wind.speed),
+              direction: data.wind.deg
+            },
+            forecast: data.weather[0].description
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }, [cityName]);
+
+  function getWeatherMessage(city, temperature, humidity, wind, forecast) {
+  
+  }
+  function getWeatherMessage(city, temperature, humidity, wind, forecast) {
+    if (temperature.current < 50) {
+      return `Looks like it's chilly in ${city} today! Don't forget to bring a jacket.`;
+    } else if (temperature.current < 30) {
+      return `Brrr! It's freezing in ${city}! Bring a heavy coat today.`;
+    } else if (forecast.includes('rain')) {
+      return `Looks like it might rain in ${city} today. Don't forget your umbrella!`;
+    } else if (temperature.current > 80 && humidity > 60) {
+      return `It's hot and humid in ${city} today! Stay hydrated and wear breathable clothing.`;
+    } else if (wind.speed > 15) {
+      return `Looks like it's windy in ${city} today! Hold onto your hats.`;
+    } else if (temperature.current > 90) {
+      return `Wow, it's scorching in ${city} today! Stay indoors or seek shade if possible.`;
+    } else if (temperature.current > 75 && forecast.includes('thunderstorms')) {
+      return `Be careful if you're out and about in ${city} today - there's a chance of thunderstorms.`;
+    } else {
+      return `Looks like a beautiful day in ${city}! Pack light and enjoy the sunshine.`;
+    }
+}
+  const handleCityChange = (event) => {
+    setCityName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleCitySelect = (event) => {
+    setCityName(event.target.value);
+  };
+
+  return (
+    <div className="weather-container">
+      <h1>{weatherData.city}, {weatherData.state}</h1>
+      <p>{weatherData.temperature.current}&deg;F</p>
+      <p>{weatherData.forecast}</p>
+      
+    <p>Humidity: {weatherData.humidity}%</p>
+      <p>Wind Speed: {weatherData.wind.speed} mph</p>
+      <p>{getWeatherMessage(weatherData.city, weatherData.temperature, weatherData.humidity, weatherData.wind, weatherData.forecast)}</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          City:
+          <input type="text" value={cityName} onChange={handleCityChange} />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+      <label>
+        Suggested Cities:
+        <select value={cityName} onChange={handleCitySelect}>
+          <option value="">--Select a city--</option>
+          {cityList.map(city => (
+            <option key={city} value={city}>{city}</option>
+          ))}
+        </select>
+      </label>
+    </div>
+  );
+  
+  
+};
+
+export default App;
