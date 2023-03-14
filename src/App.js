adfce27da166e62d14567f1fa222b5d2fb91a408
rominadouk <douk.romina@gmail.com>
2023-03-14 13:51:17 -0700

    got mckees

--- src/App.js
+++ src/App.js
@@ -3,50 +3,10 @@ import axios from 'axios'
 import './App.css';
 import Weather from './Weather';
 
-
-function App() {
-
-  //Get packed items database 
-  const allPackingItems = () => {
-    axios.get('http://localhost:3000/').then(response => {
-
-    });
-  };
-
-  //Delete
-
-
-
-  //Update
-
-
-
-  function getWeatherMessage(city, temperature, humidity, wind, forecast) {
-    if (temperature.current < 50) {
-      return `Looks like it's chilly in ${city} today! Don't forget to bring a jacket.`;
-    } else if (temperature.current < 30) {
-      return `Brrr! It's freezing in ${city}! Bring a heavy coat today.`;
-    } else if (forecast.includes('rain')) {
-      return `Looks like it might rain in ${city} today. Don't forget your umbrella!`;
-    } else if (temperature.current > 80 && humidity > 60) {
-      return `It's hot and humid in ${city} today! Stay hydrated and wear breathable clothing.`;
-    } else if (wind.speed > 15) {
-      return `Looks like it's windy in ${city} today! Hold onto your hats.`;
-    } else if (temperature.current > 90) {
-      return `Wow, it's scorching in ${city} today! Stay indoors or seek shade if possible.`;
-    } else if (temperature.current > 75 && forecast.includes('thunderstorms')) {
-      return `Be careful if you're out and about in ${city} today - there's a chance of thunderstorms.`;
-    } else {
-      return `Looks like a beautiful day in ${city}! Pack light and enjoy the sunshine.`;
-    }
-  }
- 
-
 const App = () => {
-  //use setlist Dat in handle change
-  const [listData, setListData] = useState(
+  const [listArray, setListArray] = useState(
     {
-      category:[""],
+      category:"",
       top:[""], 
       bottoms:[""],
       shoes:[""],
@@ -55,32 +15,31 @@ const App = () => {
     }
   );
 
-  const [lists, setLists] = useState([])
+  const [list, setList] = useState([])
 
   //Get packed items database 
   const allPackingItems = () => {
     axios.get('http://localhost:3000/location').then(response => {
-      setLists(response.data)
+      setListArray(response.data)
       console.log(response.data)
     });
   };
 
 
   //Delete
-  const deleteItem = (lists) => {
-    axios.delete(`http://localhost:3000/location/${lists._id}`).then(()=> {
+  const deleteItem = (listArray) => {
+    axios.delete(`http://localhost:3000/location/${listArray._id}`).then(()=> {
       axios.get('http://localhost:3000/location').then((response) => {
-        setLists(response.data)
+        setList(response.data)
       })
     })
   };
 
   useEffect(()=> {
-    allPackingItems()
+    allPackingItems();
   },[])
 
   //Update
-
 
   return (
     <div>
