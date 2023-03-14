88bf408778995304a273b23dc2211bde0af582aa
rominadouk <douk.romina@gmail.com>
2023-03-14 13:59:28 -0700

    added displayed map data

--- src/App.js
+++ src/App.js
@@ -4,47 +4,62 @@ import './App.css';
 import Weather from './Weather';
 
 const App = () => {
-  const [listArray, setListArray] = useState(
-    {
-      category:"",
-      top:[""], 
-      bottoms:[""],
-      shoes:[""],
-      headgear: [""],
-      accessories: [""]
-    }
-  );
-
   const [list, setList] = useState([])
 
-  //Get packed items database 
-  const allPackingItems = () => {
-    axios.get('http://localhost:3000/location').then(response => {
-      setListArray(response.data)
-      console.log(response.data)
-    });
-  };
-
-
-  //Delete
-  const deleteItem = (listArray) => {
-    axios.delete(`http://localhost:3000/location/${listArray._id}`).then(()=> {
-      axios.get('http://localhost:3000/location').then((response) => {
-        setList(response.data)
+    //use setlist Dat in handle change
+    const [listData, setListData] = useState(
+      {
+        category:[""],
+        top:[""], 
+        bottoms:[""],
+        shoes:[""],
+        headgear: [""],
+        accessories: [""]
+      }
+    );
+  
+    const [lists, setLists] = useState([])
+  
+    //Get packed items database 
+    const allPackingItems = () => {
+      axios.get('http://localhost:3000/location').then(response => {
+        setLists(response.data)
+        console.log(response.data)
+      });
+    };
+  
+  
+    //Delete
+    const deleteItem = (lists) => {
+      axios.delete(`http://localhost:3000/location/${lists._id}`).then(()=> {
+        axios.get('http://localhost:3000/location').then((response) => {
+          setLists(response.data)
+        })
       })
-    })
-  };
-
-  useEffect(()=> {
-    allPackingItems();
-  },[])
-
+    };
+  
+    useEffect(()=> {
+      allPackingItems()
+    },[])
   //Update
 
   return (
     <div>
       <button onClick={allPackingItems}>Get Packing Items</button>
       <Weather />
+      <h2>Packing List</h2>
+      {lists.map((list) => {
+        return (
+          <div>
+            <h3>{list.category}</h3>
+            <p>{list.top}</p>
+            <p>{list.bottoms}</p>
+            <p>{list.shoes}</p>
+            <p>{list.headgear}</p>
+            <p>{list.accessories}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
