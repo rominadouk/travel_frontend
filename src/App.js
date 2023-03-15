4fcd6457e2790fa82799ffcfdcaf8060f46292fb
rominadouk <douk.romina@gmail.com>
2023-03-15 11:18:16 -0700

    added overall layout/route setup for update route

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import axios from 'axios'
 import './App.css';
 import Weather from './Weather';
 import PackingList from './components/PackingList';
+import Update from './components/Update';
 
 
 const App = () => {
   const [list, setList] = useState([])
-
-    
+  const [updated, setUpdated] = useState(true)
+  const handleUpdate = () => {
+    setUpdated(!updated)
+  };
 
   return (
     <div>
       
       <Weather />
-      <PackingList />
+      <PackingList list={list}/>
+      <Update list={list} handleUpdate={handleUpdate} />
   
     </div>
   );
